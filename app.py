62d65864b82725cf7ee4ca0f0beb6f18adeedcce
BASSET Aymeric <aymeric.basset@icm-institute.org>
2024-12-05 14:43:25 +0100

    app

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 import sklearn
+import pandas as pd
+import numpy as np
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, classification_report, confusion_matrix
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from scipy.stats import chi2_contingency
 
+st.set_page_config(layout='wide')
 st.title("QuPath Estimator")
